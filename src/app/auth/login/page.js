9651db4f451e9b25e6574a3e29fbd776feb1e98d
linlaz <lazuardilintang@apps.ipb.ac.id>
2023-11-12 21:35:07 +0700

    onChange

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,14 +1,22 @@
 "use client";
+import React, { useState } from "react";
 import { TextInput } from "flowbite-react";
-import { useState } from "react";
 import { FcAddressBook } from "react-icons/fc";
 
 export default function Login() {
-    const [email, setEmail] = useState('')
+  const [email, setEmail] = useState('')
   return (
     <div className="container mx-auto p-4">
       <form className="max-w-md mx-auto">
-        <TextInput type="input" placeholder="Email" name="email" icon={FcAddressBook} value={email}/>
+        <TextInput
+          type="email"
+          placeholder="example@example.com"
+          name="email"
+          icon={FcAddressBook}
+          value={email}
+          onChange={(event)=>setEmail(event.target.value)}
+        />
+        
       </form>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,5 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    colors: {
-      primary: '#007BFF',     
-      secondary: '#6C757D',    
-      success: '#28A745',      
-      danger: '#DC3545',       
-      warning: '#FFC107',    
-      info: '#17A2B8',       
-    },
-
   },
 }
